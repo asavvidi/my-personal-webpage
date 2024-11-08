5c1a1717389ababffb0dd57ba5d0a663dd4a14dc
asavvidi <asavvidi@csd.auth.gr>
2024-11-08 23:14:42 +0200

    Create a CSS file for styling the Nav bar and Footer. Also, almost finished the about me page

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector(".press-button");
 
 btn.addEventListener("click", () => {
-  document.querySelector(".press-div").classList.add("hidden");
+  document.querySelector(".press-div").classList.toggle("hidden");
+  document.querySelector(".after-press-div").classList.remove("hidden");
 });
